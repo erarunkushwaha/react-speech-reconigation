2276391be313ecc660502f168aa792e8af25bc04
RashikaTarifica <rkarki@tarifica.com>
2026-03-26 21:41:55 +0545

    comment removed

--- src/components/SpeechRecognition.tsx
+++ src/components/SpeechRecognition.tsx
@@ -60,7 +60,7 @@ const SpeechRecognition = () => {
    */
   const lineIdRef = useRef(0);
 
-  // TODO: Optimize this component --> SUGGESTION BY ORACLE ..
+  // TODO: Optimize this component --> SUGGESTION BY ORACLE
 
   /**
    * useEffect runs once on mount to initialize Speech Recognition
